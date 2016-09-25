accb032f90018af784fcb3fedf192ff7e83e356b
abinkrishna <abinkrishna2004@gmail.com>
2016-09-24 21:32:04 -0700

    Initial Commit
    Signed-off-by: abinkrishna <abinkrishna2004@gmail.com>

---
+++ AK.TestAutomation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AK.TestAutomation.MvcApplication" Language="C#" %>
---
+++ AK.TestAutomation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AK.TestAutomation.Startup))]
+namespace AK.TestAutomation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
